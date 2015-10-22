abea4b7027c1f3a8306f98533272c9620fe9a9e7
Scott <dev@misonline.biz>
2015-10-22 01:21:48 -0400

    Updated dist

--- dist/json-pointer.js
+++ dist/json-pointer.js
@@ -1,3 +1,48 @@
+(function (root, factory) {
+    if (typeof module !== 'undefined' && module.exports) {
+        //commonjs
+        module.exports = factory();
+    } else if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define([], factory);
+    } else {
+        // Browser globals (root is window)
+        root._utils=root._utils || {};
+        root._utils.forEach=factory();
+        root.returnExports = root._utils.forEach;
+    }
+}(this, function () {
+    var hasOwn = Object.prototype.hasOwnProperty;
+    var toString = Object.prototype.toString;
+
+    return function forEach (obj, fn, ctx) {
+        if (toString.call(fn) !== '[object Function]') {
+            throw new TypeError('iterator must be a function');
+        }
+        var l = obj.length;
+        if (l === +l) {
+            for (var i = 0; i < l; i++) {
+                fn.call(ctx, obj[i], i, obj);
+            }
+        } else {
+            for (var k in obj) {
+                if (hasOwn.call(obj, k)) {
+                    fn.call(ctx, obj[k], k, obj);
+                }
+            }
+        }
+    };
+
+}));
+
+
+
+
+
+
+
+
+
 
 (function (root, factory) {
     if (typeof module !== 'undefined' && module.exports) {
